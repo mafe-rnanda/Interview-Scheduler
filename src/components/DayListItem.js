4a2dcfcd8b2cd8a4e2fc59dbb711cf2c1d3f967c
mafe-rnanda <mf.chavezminjarez@gmail.com>
2021-02-15 16:46:23 -0800

    Add tests and refactor code to make sure the correct text and number of spots left appear

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,26 +8,21 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected,
     "day-list__item--full": !(props.spots)
   })
+
+  const formatSpots = () => {
+    if (props.spots === 0) {
+      return "no spots remaining";
+    } else if (props.spots === 1) {
+      return "1 spot remaining"
+    } else {
+      return props.spots + " spots remaining"
+    }
+  }
   
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
-
-
-
-/*
-name:String the name of the day
-spots:Number the number of spots remaining
-selected:Boolean true or false declaring that this day is selected
-setDay:Function accepts the name of the day eg. "Monday", "Tuesday"
-
-CLASSES:
-.day-list__item { ... }
-.day-list__item--selected { ... }
-.day-list__item--full. { ... }
-
-*/
