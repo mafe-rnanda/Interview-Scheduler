0d0523127e831d5e9d3964c351cd47dead189883
mafe-rnanda <mf.chavezminjarez@gmail.com>
2021-02-16 13:03:09 -0800

    Create InterviewerListItem component, style and story

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import classNames from "classnames";
+import "components/InterviewerListItem.scss"
+
+
+export default function InterviewerListItem (props) {
+  const interviewersClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+  
+  return(
+    <li
+      className={interviewersClass}
+      onClick={props.setInterviewer}
+    >
+    <img
+      className="interviewers__item-image"
+      src={props.avatar}
+      alt={props.name}
+    />
+      {props.selected && props.name}
+    </li>
+  )
+}
+
+/*
+id:number - the id of the interviewer
+name:string - the name of the interviewer
+avatar:url - a url to an image of the interviewer
+selected:boolean - to determine if an interview is selected or not
+setInterviewer:function - sets the interviewer upon selection
+*/
