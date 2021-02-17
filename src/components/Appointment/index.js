670346284dd614a68809b8782efe6f9c82816402
mafe-rnanda <mf.chavezminjarez@gmail.com>
2021-02-16 16:51:35 -0800

    Create children components for the Appointment component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import Appointment from "components/Appointment";
+import "components/Appointment/styles.scss"
+
+export default function Appointment() {
+
+  return (
+    <article className="appointment"></article>
+  );
+}
