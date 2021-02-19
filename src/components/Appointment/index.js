ca0ada6b2277729089454992586b7c33be8b5a96
mafe-rnanda <mf.chavezminjarez@gmail.com>
2021-02-18 16:02:34 -0800

    Add the custom hook to appointments to render different modes in the application

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,21 +1,44 @@
-import React from 'react';
-import Header from 'components/Appointment/Header.js';
-import Show from 'components/Appointment/Show.js';
-import Empty from 'components/Appointment/Empty.js';
+import React from "react";
+import Header from "components/Appointment/Header.js";
+import Show from "components/Appointment/Show.js";
+import Empty from "components/Appointment/Empty.js";
+import Form from "components/Appointment/Form.js";
+import useVisualMode from "hooks/useVisualMode";
 
-import "components/Appointment/styles.scss"
+import "components/Appointment/styles.scss";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
-
+  
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+  
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? 
-        (<Show 
+      
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === CREATE && (
+        <Form
+        name={""}
+        interviewers={[]}
+        interviewer={{}}
+        onSave={() => console.log("onSave")}
+        onCancel={() => back()}
+        />
+      )}
+      {mode === SHOW && (
+        <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-        />)
-        : <Empty />}
+          onEdit={() => console.log("onEdit")}
+          onDelete={() => console.log("onDelete")}
+        />
+      )}
     </article>
   );
 }
