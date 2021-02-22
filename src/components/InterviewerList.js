ac43418381abb94cde44caf916af2949a1ec8c39
mafe-rnanda <mf.chavezminjarez@gmail.com>
2021-02-22 14:15:59 -0800

    Test prop-type is working properly

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "components/InterviewerList.scss"
 import InterviewerListItem from './InterviewerListItem';
+import PropTypes from 'prop-types';
 
 
 export default function InterviewerList (props) {
@@ -8,15 +9,15 @@ export default function InterviewerList (props) {
   const mappedList = props.interviewers.map(interviewer => {
     return (
       <InterviewerListItem
-        key={interviewer.id}
-        name={interviewer.name}
-        avatar={interviewer.avatar}
-        selected = {interviewer.id === props.value}
-        setInterviewer={event => props.onChange(interviewer.id)}
+      key={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected = {interviewer.id === props.value}
+      setInterviewer={event => props.onChange(interviewer.id)}
       />
-    )
-  })
-
+      )
+    })
+    
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
@@ -24,3 +25,7 @@ export default function InterviewerList (props) {
     </section>
   )
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
